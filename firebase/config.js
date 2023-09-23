56bcfe2e87c8acedf37c6f8b36a35a2e1763a197
MahnoorHaider <125372033+mahnoorhyder-bit@users.noreply.github.com>
2023-09-24 00:21:47 +0500

    Add files via upload

---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyD-vYr76eG4upzZwmU327h-CKkOloKZNeY",
+    authDomain: "page-c0493.firebaseapp.com",
+    projectId: "page-c0493",
+    storageBucket: "page-c0493.appspot.com",
+    messagingSenderId: "342172923995",
+    appId: "1:342172923995:web:95cead06f92d2deb6f677e",
+    measurementId: "G-3Y990VCZ0X"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
